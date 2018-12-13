60a3244956a7f9cfea8860dabb7838da8b1b4797
Juan Elosua <juan.elosua@gmail.com>
2018-12-13 22:07:37 +0100

    initial commit

---
+++ get_github_api_data.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+import datetime
+from time import sleep
+import csv
+
+
+HEADER = ["package_name", "package_url", "gist_dependencies", "repository_type",
+          "repository_url", "weekly_downloads", "dependents",
+          "contributors", "perc_participation", "commits"]
+
+cwd = os.path.dirname(__file__)
+OUTPUT_PATH = os.path.join(cwd, 'data')
+INPUT_FILE = 'npm_packages_metadata'
+OUTPUT_FILE = 'npm_packages_github_metadata'
+# Regex templates
+github_tpl_regex = re.compile('^https://github.com/(.+?)/([^/]+).*$')
+pagination_tpl_regex = re.compile('^.*\?page=(\d+)>; rel="last".*$')
+# Github api token
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN','GITHUB_TOKEN')
+
+
+def compute_perc_participation(r):
+    """
+    """
+    perc_participation = 0
+    data = r.json()
+    commits_owner = sum(data['owner'])
+    commits_all =  sum(data['all'])
+    if commits_all != 0:
+        perc_participation = (commits_all - commits_owner) * 100 / commits_all
+
+    return perc_participation
+
+
+def get_perc_participation(pkg):
+    """
+    Get the percentage of participation using Github API v3
+        - calculated as the overall commits over the overall commits minus the commits of the owner
+    """
+    participation = None
+
+    # Github API v3 list of commits
+    GITHUB_PARTICIPATION_API_TPL = "https://api.github.com/repos/%(owner)s/%(repo)s/stats/participation"
+    url = GITHUB_PARTICIPATION_API_TPL % {'owner': pkg['owner'], 'repo': pkg['repo']}
+    r = make_request(url,{'Authorization': 'token %s' % (GITHUB_TOKEN)})
+    if r.status_code == 200:
+        pkg['perc_participation'] = compute_perc_participation(r)
+    elif r.status_code == 202:
+        success = False
+        for i in range(1,6):
+            r = make_request(url,{'Authorization': 'token %s' % (GITHUB_TOKEN)})
+            if r.status_code == 200:
+                pkg['perc_participation'] = compute_perc_participation(r)
+                success = True
+                break
+            sleep(i*2)
+        if not success:
+            print 'after 5 retries we could not get cached data for %s from github....desisting' % (
+                pkg['repository_url'])
+    else:
+        print "unexpected status_code %s for repo %s" % (r.status_code, pkg['repository_url'])
+
+    return pkg
+
+
+def compute_commit_activity(r):
+    commits = 0
+    data = r.json()
+    for d in data:
+        commits += d['total']
+
+    return commits
+
+
+def get_commmit_activity(pkg):
+    """
+    """
+    tot_commits = 0
+
+    # Github API v3 list of commits
+    GITHUB_COMMIT_ACTIVITY_API_TPL = "https://api.github.com/repos/%(owner)s/%(repo)s/stats/commit_activity"
+    url = GITHUB_COMMIT_ACTIVITY_API_TPL % {'owner': pkg['owner'], 'repo': pkg['repo']}
+    r = make_request(url,{'Authorization': 'token %s' % (GITHUB_TOKEN)})
+    if r.status_code == 200:
+        pkg['commits'] = compute_commit_activity(r)
+    elif r.status_code == 202:
+        success = False
+        for i in range(1,6):
+            r = make_request(url,{'Authorization': 'token %s' % (GITHUB_TOKEN)})
+            if r.status_code == 200:
+                pkg['commits'] = compute_commit_activity(r)
+                success= True
+                break
+            sleep(i*2)
+        if not success:
+            print 'after 5 retries we could not get cached data for %s from github....desisting' % (
+                pkg['repository_url'])
+    else:
+        print "unexpected status_code for repo %s" % (pkg['repository_url'])
+
+    return pkg
+
+
+def get_contributors(pkg):
+    """
+    Get the number of contributors using Github API v3
+        - if it has more than 30 aproximate the total number flooring to the last 30
+    """
+    contributors = None
+
+    # Github API v3 list of commits
+    GITHUB_CONTRIB_API_TPL = "https://api.github.com/repos/%(owner)s/%(repo)s/contributors"
+    url = GITHUB_CONTRIB_API_TPL % {'owner': pkg['owner'], 'repo': pkg['repo']}
+    r = make_request(url,{'Authorization': 'token %s' % (GITHUB_TOKEN)})
+    if r.status_code == 200:
+        if 'link' in r.headers:
+            m = pagination_tpl_regex.match(r.headers['link'])
+            if m:
+                contributors = (int(m.group(1))-1)*30
+        else:
+            contributors = len(r.json())
+        pkg['contributors'] = contributors
+    else:
+        print "unexpected status_code for repo %s" % (pkg['repository_url'])
+
+    return pkg
+
+
+def make_request(url, headers):
+    """
+    make an http request
+    """
+    r = None
+    try:
+        r = requests.get(url, headers=headers)
+    except Exception as e:
+        raise e
+    return r
+
+
+def get_npm_metadata_results():
+    """
+    get previously calculated npm metadata results
+    """
+    try:
+        with open('%s/%s.csv' % (OUTPUT_PATH, INPUT_FILE), 'r') as f:
+            reader = csv.DictReader(f)
+            return list(reader)
+    except IOError as e:
+        return list()
+
+
+def run(args):
+    """
+    main loop
+    """
+    if not os.path.exists(OUTPUT_PATH):
+        os.makedirs(OUTPUT_PATH)
+
+    rows = get_npm_metadata_results()
+
+    with open('%s/%s.csv' %
+              (OUTPUT_PATH, OUTPUT_FILE), 'w') as fout:
+        writer = csv.DictWriter(fout, fieldnames=HEADER, extrasaction='ignore')
+        writer.writeheader()
+        count = 0
+        for row in rows:
+            if row['repository_type'] == 'github':
+                count +=1
+                if (count % 50 == 0):
+                    print('processed %s github repos' % (count))
+
+                m = github_tpl_regex.match(row['repository_url'])
+                if m:
+                    row['owner'] = m.group(1)
+                    row['repo'] = m.group(2)
+                else:
+                    print "could not extract owner and repo from github repo url %s" % (row['repository_url'])
+
+                row = get_contributors(row)
+                row = get_perc_participation(row)
+                row = get_commmit_activity(row)
+            else:
+                print 'repository type is not github for npm package %s' % (row['package_url'])
+                row['contributors'] = None
+                row['perc_participation'] = None
+                row['commits'] = None
+            writer.writerow(row)
+
+
+if __name__ == '__main__':
+    # Parse command-line arguments.
+    parser = argparse.ArgumentParser(
+        description="Get github api data")
+    args = parser.parse_args()
+    run(args)
---
+++ scrape_npm_metadata.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import re
+import requests
+from bs4 import BeautifulSoup
+import datetime
+from time import sleep
+import csv
+
+
+DAYS_DELTA = 365
+COMMITS_BOUNDARY = 5
+URL_MOST_DEPENDENT_LIBRARIES = "https://gist.githubusercontent.com/anvaka/8e8fa57c7ee1350e3491/raw/27c81e27e0ebd7331db1fd1ecf14f2179530c083/01.most-dependent-upon.md"
+md_file=""
+HEADER = ["package_name", "package_url", "gist_dependencies", "repository_type",
+          "repository_url", "weekly_downloads", "dependents"]
+
+cwd = os.path.dirname(__file__)
+OUTPUT_PATH = os.path.join(cwd, 'data')
+DATA_FILE = 'npm_packages_metadata'
+# Regex templates
+gist_tpl_regex = re.compile('^\d+\.\s\[(.*)\]\((.*)\)\s*-\s*(\d+).*$')
+dependents_tpl_regex = re.compile('^\?activeTab=dependents$')
+github_tpl_regex = re.compile('^https://github.com/[^#]+$')
+
+
+def get_most_dependent_libraries():
+    """
+    Get the most depended upon libraries from a public gist that relies on a cron job
+    https://gist.github.com/anvaka/8e8fa57c7ee1350e3491
+    """
+    try:
+        response = requests.get(URL_MOST_DEPENDENT_LIBRARIES)
+        lines = response.content.split("\n")
+        return lines
+    except Exception as e:
+        print "could not retrieve libraries from gist"
+        raise e
+
+    return md_file
+
+
+def transform_md_file(lines):
+    """
+    transform MarkDown file into a list of valid rows and initialize each row with global constants
+    """
+    data = []
+    for line in lines:
+        row={'low_maintenance_boundary': COMMITS_BOUNDARY,
+             'days_delta': DAYS_DELTA}
+        m =gist_tpl_regex.match(line)
+        if m:
+            row['package_name'] = m.group(1)
+            row['package_url'] = m.group(2)
+            row['gist_dependencies'] = m.group(3)
+            data.append(row)
+    return data
+
+
+def make_request(url, headers):
+    """
+    make an http request
+    """
+    r = None
+    try:
+        r = requests.get(url, headers=headers)
+    except Exception as e:
+        raise e
+    return r
+
+
+def scrape_npm_metadata(pkg):
+    """
+    Get package metadata from the package npm page
+        - Dependents
+        - Weekly downloads
+        - Repository url
+    """
+    r = make_request(pkg['package_url'], None)
+    # If we receive a too many requests status code sleep for 5 minutes since
+    # the site does not provide a Retry-after header and retry one more time
+    if r.status_code == 429:
+        sleep(5*60)
+        r = make_request(pkg['package_url'], None)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    # Get dependents metadata
+    dep_meta = soup.find('a', href=dependents_tpl_regex).find('span', class_='tabs__number___VComN')
+    if dep_meta:
+        pkg['dependents'] = int(dep_meta.text.replace(',',''))
+
+    # Get weekly downloads
+    dl_meta = soup.find('p', class_='package__counter___2s5Ci')
+    if dl_meta:
+        pkg['weekly_downloads'] = int(dl_meta.text.replace(',',''))
+    # Get github repo
+    gh_url = soup.find('a', class_='package__sidebarLink___zE7yA', href=github_tpl_regex)
+    if gh_url:
+        pkg['repository_type'] = 'github'
+        pkg['repository_url'] = gh_url['href']
+    else:
+        pkg['repository_type'] = 'unknown'
+        pkg['repository_url'] = None
+    return pkg
+
+def get_previous_results():
+    """
+    get previous results and use them as a cache as a polite gesture to npm servers
+    you can use the --no-cache flag to start fresh instead
+    """
+    try:
+        with open('%s/%s.csv' % (OUTPUT_PATH, DATA_FILE), 'r') as f:
+            reader = csv.DictReader(f)
+            return list(reader)
+    except IOError as e:
+        return list()
+
+
+def run(args):
+    """
+    main loop
+    """
+    prev_rows = []
+    cache = set()
+    # Restore cached package metadata if available and not overwritten by flag
+    if not args.no_cache:
+        prev_rows = get_previous_results()
+        cache = set([r['package_name'] for r in prev_rows])
+
+    # Get investigation libraries from original gist
+    # TODO: improve source by directly hitting the npm registry?
+    libraries_md = get_most_dependent_libraries()
+    # Transform from markdown to list of valid rows
+    packages = transform_md_file(libraries_md)
+
+    if not os.path.exists(OUTPUT_PATH):
+        os.makedirs(OUTPUT_PATH)
+
+    with open('%s/%s.csv' %
+              (OUTPUT_PATH, DATA_FILE), 'w') as fout:
+        writer = csv.DictWriter(fout, fieldnames=HEADER, extrasaction='ignore')
+        writer.writeheader()
+        # Write previous rows if available and we are using cache
+        writer.writerows(prev_rows)
+        count = 0
+        for pkg in packages:
+            if pkg['package_name'] in cache:
+                continue
+
+            count +=1
+            pkg = scrape_npm_metadata(pkg)
+            cache.add(pkg['package_name'])
+            writer.writerow(pkg)
+
+            if (count % 10 == 0):
+                print('processed %s npm packages' % (count))
+            sleep(1.2)
+
+
+if __name__ == '__main__':
+    # Parse command-line arguments.
+    parser = argparse.ArgumentParser(
+        description="Analize npm package data")
+    parser.add_argument('--no-cache',
+                        dest='no_cache',
+                        action='store_true')
+    args = parser.parse_args()
+    run(args)
